62314cfe3f61cd3c9c7992ccf7babbebe0aa79a1
RajaShawaiz <52911485+RajaShawaiz@users.noreply.github.com>
2023-09-11 02:12:58 +0500

    updated ui

--- static/src/pages/faucet.js
+++ static/src/pages/faucet.js
@@ -1,10 +1,24 @@
-import React from "react";
-import "./faucet.css";
-
-import Navbar from "../components/Navbar/navbar";
-import BuySection from "../components/BuySection/BuySection";
-import faucet from "../assets/faucet.png";
+import React from 'react'
+import "bulma/css/bulma.css"
+import './faucet.css'
+import Footer from '../components/Footer/footer'
+import Navbar from '../components/Navbar/navbar'
+import BuySection from '../components/BuySection/BuySection'
+import faucet from '../assets/faucet.png'
+import FaucetInfo from "../FaucetInfo/FaucetInfo";
+import FaucetQueue from "../FaucetInfo/FaucetQueue";
+import FaucetRequest from "../FaucetRequest/FaucetRequest";
+import config from "react-global-configuration";
+import configuration from "./config";
+ 
+config.set(configuration);
 const Faucet = () => {
+
+  const [lastDonation, setLastDonation] = React.useState();
+
+  const onQueued = () => {
+    setLastDonation(Date.now())
+  }
   return (
     <div className="faucet">
       <Navbar />
@@ -12,12 +26,12 @@ const Faucet = () => {
         {/* <img src={faucet} /> */}
         Cicca Testnet Faucet
       </p>
-      <p className="sub-heading">Fast and reliable . 1 Cicca/Day</p>
-      <BuySection />
-      <h4>© 2023 CiccaDefi. All rights reserved.</h4>
+      <p className="sub-heading">Fast and reliable . 10 Cicca/Day</p>
+        <BuySection onQueued={onQueued} />
+		<h4>© 2023 CiccaDefi. All rights reserved.</h4>
       {/* <Footer /> */}
     </div>
-  );
-};
+  )
+}
 
-export default Faucet;
+export default Faucet
